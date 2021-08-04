1bd5ca2617d1646d571f8e355e53cca7485d57c3
hbh <sout1217@naver.com>
2021-08-04 23:27:26 +0900

    즉시 로딩과 n+1

--- src/main/java/jpabasic/ex1hellojpa/EmManager.java
+++ src/main/java/jpabasic/ex1hellojpa/EmManager.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class EmManager {
 
@@ -33,13 +34,15 @@ public class EmManager {
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
+            // em.find 는 PK 를 찍어서 하는 것 이기 때문에 team 까지 조회하는 최적화 쿼리문을 만들어준다
+//            Member findMember = em.find(Member.class, member.getId());
 
-            System.err.println("findMember = " + findMember.getTeam().getClass()); // Proxy
+            // EAGER 로 세팅했는 데도 쿼리가 2번 나간다
+            // SQL 로 그대로 번역이 된다 그대로 사용되면 Team 을 인자로 전달해주지 않았기 때문에 Member 만 조회 된다
+            // 조회 된 후 EAGER 로딩이 적혀있다는 것을 확인 한 후 그 때 Team 을 조회하는 쿼리문이 나가 쿼리문이 2번 조회가 된다
+            // 이럴 경우 Member List 안에 10 명이 존재한다면 10번이나 조회를 더 하는 문제가 생긴다
+            List<Member> members = em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
 
-            System.err.println("--------------------------");
-            System.err.println(findMember.getTeam().getName()); // Proxy GET -> 강제 초기화
-            System.err.println("--------------------------");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabasic/ex1hellojpa/domain/member/Member.java
+++ src/main/java/jpabasic/ex1hellojpa/domain/member/Member.java
@@ -31,7 +31,7 @@ public class Member {
     @OneToMany(mappedBy = "member")
     List<Order> orders = new ArrayList<>();
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "team_id")
     private Team team;
 
